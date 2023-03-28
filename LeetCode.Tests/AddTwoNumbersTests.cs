767ec473cc300df748474021770fb469ce7e8732
JamesH <James.Harrison@serko.com>
2023-03-28 15:17:13 +1300

    Add project files.

---
+++ LeetCode.Tests/AddTwoNumbersTests.cs
@@ -0,0 +1,35 @@
+namespace LeetCode.Tests;
+
+public class AddTwoNumbersTests
+{
+    private readonly Solution _;
+
+    public AddTwoNumbersTests()
+    {
+        _ = new Solution();
+    }
+
+    [Fact]
+    public void GivenThreeDidgets()
+    {
+        var l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        var l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+        var result = _.AddTwoNumbers(l1, l2);
+
+        var expected = new ListNode(7, new ListNode(0, new ListNode(8)));
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void GivenDifferentSizes()
+    {
+        var l1 = new ListNode(2, new ListNode(4, new ListNode(9)));
+        var l2 = new ListNode(5, new ListNode(6, new ListNode(4, new ListNode(9))));
+
+        var result = _.AddTwoNumbers(l1, l2);
+
+        var expected = new ListNode(7, new ListNode(0, new ListNode(4, new ListNode(0, new ListNode(1)))));
+        result.Should().BeEquivalentTo(expected);
+    }
+}
---
+++ LeetCode/AddTwoNumbers.cs
@@ -0,0 +1,75 @@
+﻿using System.Numerics;
+
+namespace LeetCode
+{
+     public class ListNode {
+         public int val;
+         public ListNode next;
+         public ListNode(int val=0, ListNode next=null) {
+             this.val = val;
+             this.next = next;
+         }
+     }
+     
+
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var num1 = GetNumber(l1);
+            var num2 = GetNumber(l2);
+
+            var sum = num1 + num2;
+
+            var sumNode = CreateNode(sum);
+
+            return sumNode;
+        }
+        
+        private BigInteger GetNumber(ListNode l1)
+        {
+            var nums = new List<int>();
+
+            var currentNode = l1;
+            while (currentNode.next != null)
+            {
+                nums.Add(currentNode.val);
+                currentNode = currentNode.next;
+            }
+            nums.Add(currentNode.val);
+
+            nums.Reverse();
+
+            var numStr = String.Join("", nums);
+            return BigInteger.Parse(numStr);
+        }
+
+        private int GetIntFromChar(char c)
+        {
+            return (int)Char.GetNumericValue(c);
+        }
+
+        private ListNode CreateNode(BigInteger sum)
+        {
+            var arr = sum
+                .ToString()
+                .Select(c => GetIntFromChar(c))
+                .ToList();
+
+            arr.Reverse();
+
+            var firstNode = new ListNode(arr[0]);
+            var currentNode = firstNode;
+
+            for (int i = 1; i < arr.Count; i++)
+            {
+                var num = arr[i];
+                var node = new ListNode(num);
+                currentNode.next = node;
+                currentNode = node;
+            }
+            return firstNode;
+        }
+
+    }
+}
