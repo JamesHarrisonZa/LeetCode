990b96840a42ebf2bedf84d1246e0256b1ef9128
JamesH <James.Harrison@serko.com>
2023-03-31 11:37:57 +1300

    Add initial files and tests for ReverseInteger problem

---
+++ LeetCode.Tests/ReverseIntegerTests.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Tests;
+
+public class ReverseIntegerTests
+{
+    ReverseInteger reverseInteger;
+
+    public ReverseIntegerTests()
+    {
+        reverseInteger = new ReverseInteger();
+    }
+
+    [Fact]
+    public void GivenSimpleNumber()
+    {
+        var num = 123;
+
+        var result = reverseInteger.Reverse(num);
+
+        var expected = 321;
+        result.Should().Be(expected);
+    }
+}
---
+++ LeetCode/ReverseInteger.cs
@@ -0,0 +1,9 @@
+﻿namespace LeetCode;
+
+public class ReverseInteger
+{
+    public int Reverse(int x)
+    {
+        return 42;
+    }
+}
